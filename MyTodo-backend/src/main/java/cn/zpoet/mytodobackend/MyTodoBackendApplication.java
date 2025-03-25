43f10a9d5e4d8759ca8208650c1edda61b17a9ba
zpoet-coder <zpoet.personal@gmail.com>
2025-03-25 15:07:52 +0800

    first commit

---
+++ MyTodo-backend/src/main/java/cn/zpoet/mytodobackend/MyTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.zpoet.mytodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTodoBackendApplication.class, args);
+    }
+
+}
---
+++ MyTodo-backend/src/test/java/cn/zpoet/mytodobackend/MyTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zpoet.mytodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
